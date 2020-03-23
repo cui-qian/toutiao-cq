6a34a864b11af322b526153b802a1ef0928ff91d
cuiqian <780540998@qq.com>
2020-03-23 16:49:41 +0800

    登录模块-绘制表单

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,43 @@
 <template>
   <div class="login-container">
-    <el-card class="box-card">
+    <el-card class="my-card">
       <img src="../assets/logo_index.png" alt />
+      <!-- 表单项:文字说明 -->
+      <el-form>
+        <el-form-item>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input
+            v-model="loginForm.code"
+            placeholder="请输入验证码"
+            style="width:240px;margin-right:'8px"
+          ></el-input>
+          <el-button>发送验证码</el-button>
+        </el-form-item>
+        <el-form-item>
+          <el-checkbox :value="true">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width:100%">登 录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "my-login"
+  name: "my-login",
+  data() {
+    return {
+      // 登录表单数据
+      loginForm: {
+        mobile: "",
+        code: ""
+      }
+    };
+  }
 };
 </script>
 
@@ -21,8 +50,8 @@ export default {
   top: 0;
   width: 100%;
   height: 100%;
-  background: url("../assets/login_bg.jpg") no-repeat center / cover;
-  .box-card {
+  background: url("../assets/login_bg1.jpg") no-repeat center / cover;
+  .my-card {
     width: 400px;
     height: 350px;
     position: absolute;
