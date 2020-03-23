1ef8c9074f35e4d728e53ae9507c89e5a5c87526
cuiqian <780540998@qq.com>
2020-03-23 15:59:43 +0800

    登录模块-基础布局 git commit -m 登录模块-基础布局 git commit -m 登录模块-基础布局

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="container">login</div>
+  <div class="login-container">
+    <el-card class="box-card">
+      <img src="../assets/logo_index.png" alt />
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,5 +12,28 @@ export default {
 };
 </script>
 
-<style>
+<style scoped lang='less'>
+// 全屏容器
+.login-container {
+  // 让宽高直接继承窗口的宽高
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  background: url("../assets/login_bg.jpg") no-repeat center / cover;
+  .box-card {
+    width: 400px;
+    height: 350px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    img {
+      display: block;
+      width: 200px;
+      margin: 0 auto 20px;
+    }
+  }
+}
 </style>
