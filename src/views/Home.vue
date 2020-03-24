ea24fbb20a9269a55fe3d51fdb81e9fef2f5d39c
cuiqian <780540998@qq.com>
2020-03-24 15:21:14 +0800

    首页模块-头部内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,7 +6,24 @@
     <!-- 右侧主体 -->
     <el-container>
       <!-- 头部 -->
-      <el-header class="my-header">Header</el-header>
+      <el-header class="my-header">
+        <!-- 图标 -->
+        <span class="el-icon-s-fold icon"></span>
+        <!-- 文字 -->
+        <span class="text">崔总的公司</span>
+        <!-- 下拉组件 -->
+        <el-dropdown class="my-dropdown">
+          <span class="el-dropdown-link">
+            <img class="user-avatar" src="../assets/avatar.jpg" alt />
+            <span class="user-name">崔倩</span>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu>
+            <el-dropdown-item icon="el-icon-setting">个人设置</el-dropdown-item>
+            <el-dropdown-item icon="el-icon-switch-button">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
       <!-- 主体 -->
       <el-main>Main</el-main>
     </el-container>
@@ -32,6 +49,29 @@ export default {
   }
   .my-header {
     border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text {
+      vertical-align: middle;
+      padding-left: 10px;
+    }
+    .my-dropdown {
+      float: right;
+      margin-right: 20px;
+      .user-avatar {
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .user-name {
+        vertical-align: middle;
+        font-weight: 700;
+        margin-left: 5px;
+      }
+    }
   }
 }
 </style>
