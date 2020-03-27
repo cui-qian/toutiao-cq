dd682101489601fbce5b1c90d207c2db9c33a858
cuiqian <780540998@qq.com>
2020-03-27 10:47:16 +0800

    内容管理-统一配置axios默认请求头

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,14 @@
 // 导出配置好的axios提供给main.js使用
 import axios from 'axios'
 
+// 导入auth模块
+import auth from '@/utils/auth'
+
 // 进行axios的配置,将来写很多axios相关的配置
+// ---基准地址
 axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0/'
+// ---请求头
+axios.defaults.headers.Authorization = `Bearer ${auth.getUser().token}`
+
 
 export default axios
