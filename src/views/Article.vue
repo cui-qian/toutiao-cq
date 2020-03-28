bbec4b512e258edb88a4e9663b61754d5f81573e
cuiqian <780540998@qq.com>
2020-03-28 10:33:44 +0800

    内容管理-筛选条件区域布局-日期

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -9,33 +9,45 @@
           <el-breadcrumb-item>内容管理</el-breadcrumb-item>
         </el-breadcrumb>
       </div>
+
+      <!-- 表单 -->
+      <el-form label-width="80px" size="small">
+        <el-form-item label="状态:">
+          <el-radio-group v-model="reqParams.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+            <el-radio :label="4">已删除</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道:">
+          <el-select v-model="reqParams.channel_id" placeholder="请选择">
+            <el-option
+              v-for="item in channelOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期:">
+          <el-date-picker
+            is-range
+            v-model="dateArr"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" size="mini">筛选</el-button>
+        </el-form-item>
+      </el-form>
+      <!-- 筛选结果区域 -->
     </el-card>
-    <!-- 表单 -->
-    <el-form label-width="80px">
-      <el-form-item label="状态:">
-        <el-radio-group v-model="reqParams.status">
-          <el-radio :label="null">全部</el-radio>
-          <el-radio :label="0">草稿</el-radio>
-          <el-radio :label="1">待审核</el-radio>
-          <el-radio :label="2">审核通过</el-radio>
-          <el-radio :label="3">审核失败</el-radio>
-          <el-radio :label="4">已删除</el-radio>
-        </el-radio-group>
-      </el-form-item>
-      <el-form-item label="频道:">
-        <el-select v-model="reqParams.channel_id" placeholder="请选择">
-          <el-option
-            v-for="item in channelOptions"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          ></el-option>
-        </el-select>
-      </el-form-item>
-      <el-form-item label="日期:"></el-form-item>
-      <el-form-item label="状态:"></el-form-item>
-    </el-form>
-    <!-- 筛选结果区域 -->
   </div>
 </template>
 
@@ -48,7 +60,10 @@ export default {
       reqParams: {
         // 当字段的值为null的时候,这个数据不会发送个后台
         status: null,
-        channel_id: null
+        // 频道
+        channel_id: null,
+        begin_pubdate: null,
+        end_pubdate: null
       },
       // 频道下拉选项数据
       channelOptions: [
@@ -56,7 +71,10 @@ export default {
           value: 100,
           label: "科技"
         }
-      ]
+      ],
+      // 时间选择框 [起始日期,结束日期]
+      // 当日期控件选择了日期后动态给 reqParams 中 begin_pubdate 和 end_pubdate 赋值
+      dateArr: []
     };
   },
   created() {
@@ -69,4 +87,5 @@ export default {
 };
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+</style>
