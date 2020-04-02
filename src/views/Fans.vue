1ca073c58432802ce7fab455561ad0c408e3c959
cuiqian <780540998@qq.com>
2020-04-02 16:36:11 +0800

    粉丝管理-基础布局

--- src/views/Fans.vue
+++ src/views/Fans.vue
@@ -4,14 +4,59 @@
       <div slot="header">
         <my-bread>粉丝管理</my-bread>
       </div>
+      <!-- tabs组件 -->
+      <el-tabs v-model="activeName" type="card">
+        <el-tab-pane label="粉丝列表" name="list">
+          <div class="fans_list">
+            <div class="fans_item" v-for="item in 24" :key="item">
+              <el-avatar :size="80" :src="circleUrl"></el-avatar>
+              <p>tony</p>
+              <el-button type="primary" plain size="small">+关注</el-button>
+            </div>
+          </div>
+          <!-- 分页 -->
+          <el-pagination
+            style="margin-top:20px"
+            background
+            layout="prev, pager, next"
+            :total="1000"
+          ></el-pagination>
+        </el-tab-pane>
+        <el-tab-pane label="粉丝画像" name="img">2</el-tab-pane>
+      </el-tabs>
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "my-fans"
+  name: "my-fans",
+  data() {
+    return {
+      // 默认激活的tabs选项
+      activeName: "list",
+      // 测试头像数据
+      circleUrl:
+        "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+    };
+  }
 };
 </script>
 
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.fans_list {
+  .fans_item {
+    width: 120px;
+    height: 170px;
+    border: 1px dashed #ddd;
+    text-align: center;
+    padding-top: 10px;
+    display: inline-block;
+    margin-right: 30px;
+    margin-bottom: 10px;
+    p {
+      margin: 10px 0;
+    }
+  }
+}
+</style>
